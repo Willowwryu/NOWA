57b91c5c73d3b9ee80c537c4f0100459d5531dd6
Willowwryu <tuyuyou@gmail.com>
2026-01-19 13:04:51 +0900

    Initial commit

---
+++ nowAApp.swift
@@ -0,0 +1,17 @@
+//
+//  nowAApp.swift
+//  nowA
+//
+//  Created by 유재훈 on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct nowAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
